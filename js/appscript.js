ee98f16c7b8ae52a14908b00022e3ce40bcb6d6c
tung.nguyen3 <tung.nguyen3@amela.vn>
2024-07-03 18:44:53 +0700

    update app script

---
+++ js/appscript.js
@@ -0,0 +1,13 @@
+document.getElementById('sheetForm').onsubmit = function(e) {
+    e.preventDefault();
+    var formData = new FormData(document.getElementById('sheetForm'));
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://script.google.com/macros/s/AKfycbzzrJ8uAZkeijO1HlCQrS6QVa5bQmLHcTvlAwrnqLd5rwXDMg2LNfCnRvOH5bcfgh_rjQ/exec');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        alert('Data saved successfully');
+    }
+    };
+    xhr.send(new URLSearchParams(formData).toString());
+};
